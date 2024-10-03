fc9a4514150f5ee64d6eef59dee03b0dd16a4b32
Rhss <3567878896@qq.com>
2024-10-04 01:59:21 +0800

    DocumentLoader

---
+++ src/main/java/com/example/springresourcedemo/Bean/MyBean.java
@@ -0,0 +1,14 @@
+package com.example.springresourcedemo.Bean;
+
+public class MyBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
